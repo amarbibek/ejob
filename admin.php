2c1ad1443a1a69d1a85ae34a18ee8518eea4dcd1
iminfinity <this.is.me.infinity@gmail.com>
2020-06-04 18:24:55 +0530

    adding admin-sidebar.php

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@ TODO:
 1.    display all jobs in tabular format and add last column with header "Actions",
 2.    this column should contain two links edit,delete 
 3.    edit and delete code I'll do
+4. wtf is this jgbhjbjhnjnhjbhghghghbgjbh
 -->
 <?php
 include("./includes/header.php");
@@ -11,150 +12,86 @@ include("./filters/functions.php");
 check_admin_login();
 
 ?>
-<!-- <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Admin</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
-    <link href="adminpanel.css" rel="stylesheet" type="text/css" />
-    <link rel="shortcut icon" href="img/favicon.ico" />
-  </head>
-  <body>
-    	<header>
-			<nav>
-  		  <ul id='navigation'>
-          <li>	<span><a href="index.html"><img id="logo" src="img/logo1.png"/></a>	</span></li>   
-				</ul>
-			</nav>
-		</header> -->
+
   <?php
-  if(isset($_POST['add-job-btn'])){
-    $job_title = $_POST['job-title'];
-    $job_detail = $_POST['job-detail'];
-    $category = $_POST['category'];
-    $sub_category = $_POST['sub-category'];
+    if(isset($_POST['add-job-btn'])){
+      $job_title = $_POST['job-title'];
+      $job_detail = $_POST['job-detail'];
+      $category = $_POST['category'];
+      $sub_category = $_POST['sub-category'];
 
-    //file upload path
-$targetDir = "./pdf/";
-$fileName = basename($_FILES["file"]["name"]);
-$targetFilePath = $targetDir . $fileName;
-$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-// $file_name=$_FILES['file']['name'];
+    $targetDir = "./pdf/";
+    $fileName = basename($_FILES["file"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+    
 
-if(isset($_POST["add-job-btn"]) && !empty($_FILES["file"]["name"])) {
-    //allow certain file formats
-    $allowTypes = array('jpg','png','jpeg','gif','pdf');
-    if(in_array($fileType, $allowTypes)){
-        //upload file to server
-        if(move_uploaded_file($_FILES["file"]["tmp_name"],__DIR__. $targetDir . $_FILES["file"]['name'])){// $targetFilePath)){
-            $statusMsg = "The file ".$fileName. " has been uploaded.";
+    if(isset($_POST["add-job-btn"]) && !empty($_FILES["file"]["name"])) {
+        //allow certain file formats
+        $allowTypes = array('jpg','png','jpeg','gif','pdf');
+        if(in_array($fileType, $allowTypes)){
+            //upload file to server
+            if(move_uploaded_file($_FILES["file"]["tmp_name"],__DIR__. $targetDir . $_FILES["file"]['name'])){
+                $statusMsg = "The file ".$fileName. " has been uploaded.";
+            }else{
+                $statusMsg = "Sorry, there was an error uploading your file.";
+            }
         }else{
-            $statusMsg = "Sorry, there was an error uploading your file.";
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
         }
     }else{
-        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+        $statusMsg = 'Please select a file to upload.';
     }
-}else{
-    $statusMsg = 'Please select a file to upload.';
-}
-
-//display status message
-// echo $statusMsg;
-    
-    
-    // $targetfolder = "pdfs/";
-    // $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
-    // $ok=1;
-    // $file_type=$_FILES['file']['type'];
-    // $file_name=$_FILES['file']['name'];
-    // // echo $file_type;
-    // // echo $file_name;
-    // if ($file_type=="application/pdf" || $file_type=="image/jpeg") {
-    //   if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
-    //   {
-    //     echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-    //   }
-    //   else {
-    //     echo "Problem uploading file";
-    //   }
-    // }
-    // else {
-    //   echo "You may only upload PDFs or JPEGs files.<br>";
-    // }
-
 
     $url = $targetDir . $fileName;
-    // echo $url;
-    // insert query
+ 
     $query="INSERT INTO `jobs` (`institute`,`job_description`,`job_sub_category_id`,`pdf_url`,`is_visible`,`created_by`) VALUES ('$job_title','$job_detail','$sub_category','$url',1,2)";
-    // echo $query;
 
-    // $result = ;
     if($conn->query($query) === TRUE){
-      // if($_POST['event'] == 'button_clicked'){
-      // echo 'job uploaded successfully!'; 
-      echo '<script type="text/javascript">',
+       echo '<script type="text/javascript">',
             'display_success_toast();',
              '</script>';
-      // }
-      // echo '<script>
-      // $.toast({
-      //     heading: "Job Added",
-      //     text: "Job has been added successfully!",
-      //     position: "top-right",
-      //     icon: "success",
-      //     stack: true
-      // });
-      // </script>';
-    }
-  }
-
+        }
+      }
   ?>
     <div class="wrapper"> 
-    <div class="job-container"> 
- 
-    <?php
-    // $category_id=$_GET['category'];
-    $initial_page_no=$_GET['page_no']; 
-    $page_no=(($initial_page_no-1)*10);
-    // $query="SELECT * FROM `jobs` LIMIT 10";
-    // $query="SELECT * FROM `jobs` WHERE `job_sub_category_id`=$category_id AND `is_visible`=1 LIMIT 10 OFFSET $page_no";
-    $query="SELECT * FROM `jobs` WHERE `is_visible`=1 LIMIT 10 OFFSET $page_no";
-		$result = $conn->query($query);
+      <div class="job-container"> 
+        <?php
+       
+          $initial_page_no=$_GET['page_no']; 
+          $page_no=(($initial_page_no-1)*10);
+       
+          $query="SELECT * FROM `jobs` WHERE `is_visible`=1 LIMIT 10 OFFSET $page_no";
+      		$result = $conn->query($query);
 
-			if ($result->num_rows > 0) { 
-			while($row = $result->fetch_assoc()) {
-				// echo '<div class="jobs">';
-				// echo '<div class="job-title"><h2><a href="#">'. $row["institute"].'</a></h2></div>';
-				// echo '<div class="job-title"><h2><a href="#">'. $row["job_description"].'</a></h2></div>';
-        // echo '</div>';
-        echo '<div class="jobs">';
-        echo '  <div class="job-title">';
-        echo '    <h2><a href="#">'. $row["institute"].'</a></h2>';
-        echo '  </div>';
-        echo '  <div class="job-details">';
-        echo '    <h3>'. $row["job_description"].'</h3>';
-        echo '  </div>';
-        if($row['pdf_url']!=NULL){
-          echo '<div class="job1-details"><a href="'.$row["pdf_url"].'" target="_blank">PDF</a></div>';
-        }
-        echo '  <div class="edit-delete">';
-        echo '    <button name="edit" class="edit-button" data-job_id="'.$row['job_id'].'" onclick="edit_job(this)">Edit</button>';
-        echo '    <button name="delete" class="delete-button" data-job_id="'.$row['job_id'].'" onclick="delete_job(this)">';
-        echo '      Delete';
-        echo '    </button>';
-        echo '  </div>';
-        echo '</div>';
-        
-			}
-			} else {
-			echo "0 results";
-			}
-    ?>
-    </div>
-       <div id='job-entry'>
+      			if ($result->num_rows > 0) { 
+      			while($row = $result->fetch_assoc()) {
+      		
+              echo '<div class="jobs">';
+              echo '  <div class="job-title">';
+              echo '    <h2><a href="#">'. $row["institute"].'</a></h2>';
+              echo '  </div>';
+              echo '  <div class="job-details">';
+              echo '    <h3>'. $row["job_description"].'</h3>';
+              echo '  </div>';
+              if($row['pdf_url']!=NULL){
+                echo '<div class="job1-details"><a href="'.$row["pdf_url"].'" target="_blank">PDF</a></div>';
+              }
+              echo '  <div class="edit-delete">';
+              echo '    <button name="edit" class="edit-button" data-job_id="'.$row['job_id'].'" onclick="edit_job(this)">Edit</button>';
+              echo '    <button name="delete" class="delete-button" data-job_id="'.$row['job_id'].'" onclick="delete_job(this)">';
+              echo '      Delete';
+              echo '    </button>';
+              echo '  </div>';
+              echo '</div>';
+              
+      			}
+      			} else {
+      			echo "0 results";
+      			}
+        ?>
+      </div>
+      <div id='job-entry'>
           <form id='add-job' action="#" method="POST" enctype="multipart/form-data">
             <h2> Enter New Job Here </h2>
             <label id='label-job-title' name='label-job-title'> Job Title: </label> <br />
@@ -241,8 +178,8 @@ if(isset($_POST["add-job-btn"]) && !empty($_FILES["file"]["name"])) {
         <a href="admin.php?page_no=<?php echo $initial_page_no-1; ?>"><< Previous</a>
         <a href="admin.php?&page_no=<?php echo $initial_page_no+1; ?>">Next >></a>
     </div>
-  <!-- </body>
-</html> -->
+
+
 <script>
 var sub_cat_id=0;
 $(function(){
---
+++ includes/admin-sidebar.php
@@ -0,0 +1,17 @@
+<button type="button" class="btn btn-default" aria-label="Left Align" id="menu-toggle">
+    <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+
+</button> 
+ <div id="wrapper">
+<div id="sidebar-wrapper">
+
+    <ul class="sidebar-nav1"> 
+        <li><a href="requests.php">REQUESTS</a></li>
+        <li><a href="add_admin.php">ADD ADMIN</a></li>
+        <li><a href="view_users.php">VIEW USERS</a></li>
+        <li><a href="view_all_cargos.php">CARGOS</a></li>
+        <li><a href="view_feedbacks.php">FEEDBACKS</a></li>
+    </ul>
+</div>
+</div>
